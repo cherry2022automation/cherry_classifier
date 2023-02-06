a7e900c2badd4356ba5ebe5cb5a91937e1029d99
cherry1 <cherry2022automation@gmail.com>
2023-02-06 14:21:54 +0900

    電磁弁数 2→8

--- solenoid_valve.py
+++ solenoid_valve.py
@@ -4,7 +4,7 @@ from tkinter import messagebox, ttk
 from tkinter import *
 import Relay
 
-from pyparsing import col
+# from pyparsing import col
 
 class solenoid_valve_control(Frame):
 
@@ -17,6 +17,7 @@ class solenoid_valve_control(Frame):
         self.pack()
 
         font_size = 20
+        sv_num = 8
 
         self.label1 = ttk.Label(self, text="on time", padding=(5,2))
         self.label1.grid(row=0, column=1, sticky=E)
@@ -29,49 +30,57 @@ class solenoid_valve_control(Frame):
         self.label2 = ttk.Label(self, text="[ms]", padding=(5,2))
         self.label2.grid(row=0, column=3, sticky=W)
 
-        self.label3 = ttk.Label(self, text="SV1", padding=(5,2))
-        self.label3.grid(row=1, column=0, sticky=E)
 
-        self.label4 = ttk.Label(self, text="SV2", padding=(5,2))
-        self.label4.grid(row=2, column=0, sticky=E)
+        self.label_sv_num = []
+        self.button_pulse = []
+        self.button_on = []
+        self.button_off = []
 
-        self.button_sv1 = ttk.Button(self, text="Pulse", command=lambda:self.sv_pulse(1,int(self.on_time_ms.get())))
-        self.button_sv1.grid(row=1, column=1)
 
-        self.button_sv2 = ttk.Button(self, text="Pulse", command=lambda:self.sv_pulse(2,int(self.on_time_ms.get())))
-        self.button_sv2.grid(row=2, column=1)
+        for i in range(8):
 
-        self.button_sv1_on = ttk.Button(self, text="ON", command=lambda:self.sv_on(1))
-        self.button_sv1_on.grid(row=1, column=2)
+            self.label_sv_num.append(ttk.Label(self, text="SV"+str(i+1), padding=(5,2)))
+            self.label_sv_num[i].grid(row=i+1, column=0, sticky=E)
 
-        self.button_sv1_off = ttk.Button(self, text="OFF", command=lambda:self.sv_off(1))
-        self.button_sv1_off.grid(row=1, column=3)
+            # self.button_pulse.append(ttk.Button(self, text="Pulse", command=lambda:self.sv_pulse(i_num,int(self.on_time_ms.get()))))
+            self.button_pulse.append(ttk.Button(self, text="Pulse", command=self.sv_pulse(i)))
+            self.button_pulse[i].grid(row=i+1, column=1, sticky=E)
+            
+            self.button_on.append(ttk.Button(self, text="ON", command=self.sv_on(i)))
+            self.button_on[i].grid(row=i+1, column=2)
 
-        self.button_sv2_on = ttk.Button(self, text="ON", command=lambda:self.sv_on(2))
-        self.button_sv2_on.grid(row=2, column=2)
+            self.button_off.append(ttk.Button(self, text="OFF", command=self.sv_off(i)))
+            self.button_off[i].grid(row=i+1, column=3)
 
-        button_sv2_off = ttk.Button(self, text="OFF", command=lambda:self.sv_off(2))
-        button_sv2_off.grid(row=2, column=3)
+    def sv_pulse(self, ch):
+        ch = ch+1
+        def x():
+            on_time_ms = int(self.on_time_ms.get())
+            try:
+                on_time_s = on_time_ms/1000
+                Relay.pulse(ch, on_time_s)
+            except:
+                messagebox.showinfo("エラー", "・「on time」の入力が半角数字になっているか\n・USBリレーが接続されているか\n確認してください")
+        return x
 
-    def sv_pulse(self, ch, on_time_ms):
-        try:
-            on_time_s = on_time_ms/1000
-            Relay.pulse(ch, on_time_s)
-        except:
-            messagebox.showinfo("エラー", "・「on time」の入力が半角数字になっているか\n・USBリレーが接続されているか\n確認してください")
 
     def sv_on(self, ch):
-        try:
-            Relay.on(ch)
-        except:
-            messagebox.showinfo("エラー", "USBリレーが接続されているか確認してください")
+        ch = ch+1
+        def x():
+            try:
+                Relay.on(ch)
+            except:
+                messagebox.showinfo("エラー", "USBリレーが接続されているか確認してください")
+        return x
 
     def sv_off(self, ch):
-        try:
-            Relay.off(ch)
-        except:
-            messagebox.showinfo("エラー", "USBリレーが接続されているか確認してください")
-
+        ch = ch+1
+        def x():
+            try:
+                Relay.off(ch)
+            except:
+                messagebox.showinfo("エラー", "USBリレーが接続されているか確認してください")
+        return x
 
 if __name__ == "__main__":
 
