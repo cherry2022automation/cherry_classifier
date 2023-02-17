c902b3ab8afdec185eeeef0e27e636e589a2ec3c
cherry1 <cherry2022automation@gmail.com>
2023-02-18 03:15:03 +0900

    設定追加

--- cherry_classifier_ver2.py
+++ cherry_classifier_ver2.py
@@ -1,14 +1,19 @@
 import cherry_classfier as cc
 import tkinter
 import Relay
+import solenoid_valve
 
 Relay.R_name = "USBRelay"
+
+cc.solenoid_valve_control.sv_num = 8
 
 class Application_ver2(cc.Application):
 
     oder_T = 3
     oder_F = 0
     oder_R = 1
+
+    delay = {"tokushu":1.9, "shu":3}
 
 if __name__ == "__main__":
 
